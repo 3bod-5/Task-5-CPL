9f57605944b20f083469009971f8f40767047002
abod <abodelking0@gmail.com>
2021-11-14 18:28:25 +0200

    reverse

--- revrse.cpp
+++ revrse.cpp
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 void reverse(char * word){
-
+	char letter;
    printf("Enter a string : \n");
    gets(word);
-
-   strrev(word);
-
-   printf("Reverse of the string you entered : %s\n", word);
+   int length;
+   length=strlen(word);
+   for(int i=length-1;i>=0;i--){
+   	letter=word[i];
+   	printf("%c", letter);
+   }
 }
 int main()
 {
